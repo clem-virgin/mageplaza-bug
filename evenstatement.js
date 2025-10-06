4ec3320268616114d4673f5a5842bc0bd1c4c19f
clem-virgin <clementvirgin02@gmail.com>
2025-10-06 07:18:34 -0400

    first commit

---
+++ evenstatement.js
@@ -0,0 +1,11 @@
+const number = 60 % 2
+const Number = 60%2 === 0
+
+console.log(number)
+console.log(Number)
+if (number <= 0){
+    console.log( "even number" )
+}
+else{
+    console.log("odd number")
+}
---
+++ oddstatement.js
@@ -0,0 +1,12 @@
+const number = 61 % 2
+const Number = 61 % 2 === 0
+
+console.log(number)
+console.log(Number)
+if (number >= 0.0001){
+    console.log("odd number")
+}
+
+else {
+    console.log("even number")
+}
